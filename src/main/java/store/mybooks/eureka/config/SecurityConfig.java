a61f7ffb7463bec60ba3ebb03923419f8aeb7915
minsu11 <zmfjtnl1@naver.com>
2024-12-03 10:38:56 +0900

    refactor: properties 파일 수정

--- src/main/java/store/mybooks/eureka/config/SecurityConfig.java
+++ src/main/java/store/mybooks/eureka/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package store.mybooks.eureka.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -23,12 +25,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request ->
+                    request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
 
